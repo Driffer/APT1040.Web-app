5077d16720a04f9a86637559b2a83bff321b8058
Driffer <136809929+Driffer@users.noreply.github.com>
2024-11-12 14:02:14 +0300

    Add files via upload

---
+++ user,script.js
@@ -0,0 +1,16 @@
+
+function contactForm(){
+    document.getElementById("qontact").innerHTML=getElementById("qontactForm");//innerHTML=forms["qontactForm"];
+}//document.getElementById("qontact").appendChild(button)+goBack();//+qontactForm
+
+
+function hideit() {
+document.getElementById("contact").style.display='none'
+}
+function Hide() {
+    document.getElementsByTagName("section").style.display='none'
+    }
+function goBack() {
+window.history.back()
+}
+   
