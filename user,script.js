f116a323c20b4eabf79b50e77998b482793cd834
Driffer <136809929+Driffer@users.noreply.github.com>
2024-11-27 06:55:46 +0300

    Add files via upload

--- user,script.js
+++ user,script.js
@@ -7,9 +7,35 @@ function contactForm(){
 function hideit() {
 document.getElementById("contact").style.display='none'
 }
-function Hide() {
+/*function Hide() {
     document.getElementsByTagName("section").style.display='none'
     }
+function navigation{
+    const routes = [
+        { path: '/', name: 'Home', component: Home },
+        { path: '/profile', name: 'Profile', component: Profile },
+        { path: '/explore', name: 'Explore', component: Explore },
+       
+      ];
+}
+    */
+function Toggle(){
+const flashcards = [
+    { question: "What is the capital of France?", answer: "Paris" },
+    { question: "Who painted the Mona Lisa?", answer: "Leonardo da Vinci" },
+    // ...
+  ];
+  
+const flashcard = document.getElementById("flashcard-container");
+const flashcardFront = document.getElementById("flashcard-front");
+const flashcardBack = document.getElementById("flashcard-back");
+
+flashcard.addEventListener("click", () => {
+  flashcardFront.style.display === "block"
+    ? (flashcardFront.style.display = "none", flashcardBack.style.display = "block")
+    : (flashcardFront.style.display = "block", flashcardBack.style.display = "none");
+})
+} 
 function goBack() {
 window.history.back()
 }
